d9e71223f523e32ef6392f83e3e7e3484e88112c
kstagaman <kstagaman@gmail.com>
2016-12-02 10:28:07 -0800

    Added option to choose bowtie or bowtie2 for merging with master branch

--- Support_scripts/rdp_tax2phyloseq_tax_nosupp.pl
+++ Support_scripts/rdp_tax2phyloseq_tax_nosupp.pl
@@ -28,6 +28,7 @@ else {
 
 	open OUT, ">$outFile" or die "\n\tError: cannot create $outFile\n\n";
 	print OUT "\tDomain\tPhylum\tClass\tSubclass\tOrder\tSuborder\tFamily\tGenus\n";
+	open TEST, ">test_file.txt" or die "\n\tError: cannot create test_file.txt\n\n";
 
 	open INF, "<$inFile" or die "\n\tError: cannot open $inFile\n\n";
 	while (<INF>) {
@@ -43,16 +44,25 @@ else {
 			) = ('NA') x 8;
 		# print "$domain\t$domain_sup\t$phylum\t$phylum_sup\t$class\t$class_sup\t$subclass\t$subclass_sup\t";
 		# print "$order\t$order_sup\t$suborder\t$suborder_sup\t$family\t$family_sup\t$genus\t$genus_sup\n";
+		$_ =~ s/\"//g;
+		print TEST "$_";
 
 		($taxon)    = /^(OTU\d+)/;
-		($domain)   = /\"*([\w\ ]+)\"*\tdomain/   unless ($_ !~ /domain/);
-		($phylum)   = /\"*([\w\ ]+)\"*\tphylum/   unless ($_ !~ /phylum/);
-		($class)    = /\"*([\w\ ]+)\"*\tclass/    unless ($_ !~ /class/);
-		($subclass) = /\"*([\w\ ]+)\"*\tsubclass/ unless ($_ !~ /subclass/);
-		($order)    = /\"*([\w\ ]+)\"*\torder/    unless ($_ !~ /order/);
-		($suborder) = /\"*([\w\ ]+)\"*\tsuborder/ unless ($_ !~ /suborder/);
-		($family)   = /\"*([\w\ ]+)\"*\tfamily/   unless ($_ !~ /family/);
-		($genus)    = /\"*([\w\ ]+)\"*\tgenus/    unless ($_ !~ /genus/);
+		($domain)   = /\d\t([\w\ \/\-]+)\tdomain/   unless ($_ !~ /domain/);
+		($phylum)   = /\d\t([\w\ \/\-]+)\tphylum/   unless ($_ !~ /phylum/);
+		if ($phylum eq "NA") {$phylum = "${domain}_phylum"}
+		($class)    = /\d\t([\w\ \/\-]+)\tclass/    unless ($_ !~ /class/);
+		if ($class eq "NA") {$class = "${phylum}_class"}
+		($subclass) = /\d\t([\w\ \/\-]+)\tsubclass/ unless ($_ !~ /subclass/);
+		if ($subclass eq "NA") {$subclass = "${class}_subclass"}
+		($order)    = /\d\t([\w\ \/\-]+)\torder/    unless ($_ !~ /order/);
+		if ($order eq "NA") {$order = "${subclass}_order"}
+		($suborder) = /\d\t([\w\ \/\-]+)\tsuborder/ unless ($_ !~ /suborder/);
+		if ($suborder eq "NA") {$suborder = "${order}_suborder"}
+		($family)   = /\d\t([\w\ \/\-]+)\tfamily/   unless ($_ !~ /family/);
+		if ($family eq "NA") {$family = "${suborder}_family"}
+		($genus)    = /\d\t([\w\ \/\-]+)\tgenus/    unless ($_ !~ /genus/);
+		if ($genus eq "NA") {$genus = "${family}_genus"}
 
 		print OUT "$taxon\t$domain\t$phylum\t$class\t$subclass\t$order\t$suborder\t$family\t$genus\n";
 	}
